75a7214dc18a3ddfb8127c434aa1c263caba01a8
Oguz Ozsahin <oguzozshn@gmail.com>
2026-04-24 09:56:27 +0300

    AmmoBox class added

---
+++ src/AmmoBox.java
@@ -0,0 +1,62 @@
+import java.util.Random;
+
+public class AmmoBox {
+    private int x;
+    private int y;
+    private double speed;
+    private int hitboxWidth;
+    private int hitboxHeight;
+    private Random rand = new Random();
+
+    public AmmoBox(int startX, int startY) {
+        x = startX;
+        y = startY;
+        speed = 1.0;
+        hitboxWidth = 30;
+        hitboxHeight = 30;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void move() {
+        y -= speed;
+    }
+
+    public boolean isOffScreen() {
+        return y < 0;
+    }
+
+    public int getHitboxWidth() {
+        return hitboxWidth;
+    }
+
+    public int getHitboxHeight() {
+        return hitboxHeight;
+    }
+
+    public double getTopLeftCornerX() {
+        return x - hitboxWidth / 2.0;
+    }
+
+    public double getTopLeftCornerY() {
+        return y + hitboxHeight / 2.0;
+    }
+
+    public double getBottomRightCornerX() {
+        return x + hitboxWidth / 2.0;
+    }
+
+    public double getBottomRightCornerY() {
+        return y - hitboxHeight / 2.0;
+    }
+
+    public int getAmmoAmount() {
+        return rand.nextInt(5) + 1; // 1-5 arası rastgele
+    }
+}
