54ba14bc273868da6644a0d51b76960fc4b03dde
Oguz Ozsahin <oguzozshn@gmail.com>
2026-04-26 20:49:17 +0300

    Bullet javadoc added

--- src/Bullet.java
+++ src/Bullet.java
@@ -5,6 +5,11 @@ public class Bullet {
     private int hitboxWidth;
     private int hitboxHeight;
 
+    /**
+     * Constructor for the Bullet class.
+     * @param startX
+     * @param startY
+     */
     public Bullet(int startX, int startY) {
         x = startX;
         y = startY;
@@ -13,46 +18,72 @@ public class Bullet {
         hitboxHeight = 10;
     }
 
+    /**
+     * Get the x-coordinate of the bullet.
+     * @return
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Get the y-coordinate of the bullet.
+     * @return
+     */
     public int getY() {
         return y;
     }
 
+    /**
+     * Move the bullet upwards.
+     */
     public void move() {
         y += speed;
     }
 
+    /**
+     * Check if the bullet is off the screen.
+     * @return
+     */
     public boolean isOffScreen() {
         return y > 600;
     }
 
-    public int getHitboxWidth() {
-        return hitboxWidth;
-    }
-
-    public int getHitboxHeight() {
-        return hitboxHeight;
-    }
-
+    /**
+     * Get the x-coordinate of the top-left corner of the bullet's hitbox.
+     * @return
+     */
     public double getTopLeftCornerX() {
         return x - hitboxWidth / 2.0;
     }
 
+    /**
+     * Returns the y-coordinate of the top-left corner of the bullet's hitbox.
+     * @return the y-coordinate of the top-left corner of the hitbox.
+     */
     public double getTopLeftCornerY() {
         return y + hitboxHeight / 2.0;
     }
 
+    /**
+     * Get the x-coordinate of the bottom-right corner of the bullet's hitbox.
+     * @return the x-coordinate of the bottom-right corner of the hitbox.
+     */
     public double getBottomRightCornerX() {
         return x + hitboxWidth / 2.0;
     }
 
+    /**
+     * Get the y-coordinate of the bottom-right corner of the bullet's hitbox.
+     * @return the y-coordinate of the bottom-right corner of the hitbox.
+     */
     public double getBottomRightCornerY() {
         return y - hitboxHeight / 2.0;
     }
-    
+
+    /**
+     * Draw the bullet on canvas.
+     */
     public void draw(){
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.filledCircle(this.getX(), this.getY(), 5);
