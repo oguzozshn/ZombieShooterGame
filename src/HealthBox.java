448e2b8a8426a7ab7fe2e7b76d13d60bfb69c283
Oguz Ozsahin <oguzozshn@gmail.com>
2026-04-26 20:52:55 +0300

    Healtbox javadoc addded

--- src/HealthBox.java
+++ src/HealthBox.java
@@ -8,6 +8,11 @@ public class HealthBox {
     private int hitboxWidth;
     private int hitboxHeight;
 
+    /**
+     * Constructor for the HealthBox class.
+     * @param startX
+     * @param startY
+     */
     public HealthBox(int startX, int startY) {
         x = startX;
         y = startY;
@@ -16,50 +21,65 @@ public class HealthBox {
         hitboxHeight = 30;
     }
 
+    /**
+     * Get the x-coordinate of the health box.
+     * @return x-coordinate
+     */
     public int getX() {
         return x;
     }
 
+    /**
+     * Get the y-coordinate of the health box.
+     * @return y-coordinate
+     */
     public int getY() {
         return y;
     }
 
-    public void move() {
-        y -= speed;
-    }
-
-    public boolean isOffScreen() {
-        return y < 0;
-    }
-
-    public int getHitboxWidth() {
-        return hitboxWidth;
-    }
-
-    public int getHitboxHeight() {
-        return hitboxHeight;
-    }
-
+    /**
+     * Get the top-left corner x-coordinate of the health box's hitbox.
+     * @return top-left corner x-coordinate
+     */
     public double getTopLeftCornerX() {
         return x - hitboxWidth / 2.0;
     }
 
+    /**
+     * Get the top-left corner y-coordinate of the health box's hitbox.
+     * @return top-left corner y-coordinate
+     */
     public double getTopLeftCornerY() {
         return y + hitboxHeight / 2.0;
     }
 
+    /**
+     * Get the bottom-right corner x-coordinate of the health box's hitbox.
+     * @return bottom-right corner x-coordinate
+     */
     public double getBottomRightCornerX() {
         return x + hitboxWidth / 2.0;
     }
 
+    /**
+     * Get the bottom-right corner y-coordinate of the health box's hitbox.
+     * @return bottom-right corner y-coordinate
+     */
     public double getBottomRightCornerY() {
         return y - hitboxHeight / 2.0;
     }
 
+    /**
+     * Get the health amount of the health box.
+     * @return health amount
+     */
     public int getHealthAmount() {
-        return 1; // Her kutudan 1 can
+        return 1;
     }
 
+    /**
+     * Draws the health box on the canvas.
+     */
     public void draw(){
         StdDraw.setPenColor(StdDraw.RED);
         StdDraw.filledSquare(this.getX(), this.getY(), 15);
